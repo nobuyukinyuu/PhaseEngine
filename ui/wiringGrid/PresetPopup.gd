049509177fab6e0efada3df678b0734b613dd290
nobuyuki_nyuu <nobuyuki.nyuu@gmail.com>
2021-08-18 00:57:58 -0500

    PresetPopup.gd:  4-op algorithm descriptions

--- ui/wiringGrid/PresetPopup.gd
+++ ui/wiringGrid/PresetPopup.gd
@@ -5,6 +5,22 @@ var TILE_SIZE = Vector2.ONE * 64
 enum AlgType {FOUR_OP=4, SIX_OP=6}
 export(AlgType) var intent=AlgType.SIX_OP
 
+const alg_descs = {4: [
+						"Four serial connections.",
+						"One carrier and one modulator serially connected\nto dual modulators at the top of the stack.",
+						"One carrier with dual modulators.\nOne modulator is stacked with another operator.",
+						"One carrier with dual modulators.\nBoth modulators are stacked by a third modulator.",
+						"Three parallel modulators on one carrier.",
+						"Two carriers; one is stacked with 2 serial modulators.",
+						"Two carriers; both are stacked by the same modulator chain.",
+						"Two carriers with one modulator each.",
+						"Three parallel carriers sharing a common modulator.",
+						"Three parallel carriers, with two sharing a common modulator.",
+						"Three parallel carriers, with one having a modulator.",
+						"Four parallel carriers.  (Additive sine synthesis)",
+]
+}
+
 func _ready():
 #	create_context()
 	pass
@@ -23,7 +39,8 @@ func create_context():
 		icon.region = Rect2(origin + xy*TILE_SIZE, TILE_SIZE)
 
 		list.add_item(str(i+1), icon)
-	
+		if intent==AlgType.FOUR_OP:
+			list.set_item_tooltip(i, "Algorithm %s: \n%s" % [ i, alg_descs[4][i] ])
 
 
 func _on_Popup_about_to_show():
