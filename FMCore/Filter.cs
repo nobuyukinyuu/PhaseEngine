6ff4c53647360858f73ced165e344f967d543983
nobuyuki_nyuu <nobuyuki.nyuu@gmail.com>
2021-10-25 11:30:35 -0500

    Small Filter refactor;  Testing fast lerp

--- FMCore/Filter.cs
+++ FMCore/Filter.cs
@@ -47,8 +47,7 @@ namespace gdsFM
         public override void SetOscillatorType(byte index)
         {
             eg.aux_func = index;
-            Recalc((FilterType)index, eg.cutoff, eg.resonance, 0);
-            // throw new NotImplementedException();
+            Recalc();
         }
 
         public float Process(float in0)
@@ -75,146 +74,118 @@ namespace gdsFM
         public void Recalc(FilterType type, double frequency, double q, double db_gain, bool q_is_bandwidth=false)
         {
             // temp pi
-            const double temp_pi=3.1415926535897932384626433832795;
+            const double PI=3.1415926535897932384626433832795;
             // temp coef vars
             double alpha=0,a0=0,a1=0,a2=0,b0=0,b1=0,b2=0;
 
-            // peaking, lowshelf and hishelf
+            double omega=	2.0*PI*frequency/Global.MixRate;
+            double tsin	=	Math.Sin(omega);
+            double tcos	=	Math.Cos(omega);
+
+            if(q_is_bandwidth)
+                alpha=tsin * Math.Sinh(Math.Log(2.0)/2.0*q*omega/tsin);
+            else
+                alpha=tsin/(2.0*q);
+
+            double A=0, beta=0;
+            // for peaking, lowshelf and hishelf
             if((int)type>6)
             {
-                double A	=	Math.Pow(10.0,(db_gain/40.0));
-                double omega=	2.0*temp_pi*frequency/Global.MixRate;
-                double tsin	=	Math.Sin(omega);
-                double tcos	=	Math.Cos(omega);
-                
-                if(q_is_bandwidth)
-                alpha=tsin * Math.Sinh(Math.Log(2.0)/2.0*q*omega/tsin);
-                else
-                alpha=tsin/(2.0*q);
+                A   	=	Math.Pow(10.0,(db_gain/40.0));
+                beta	=	Math.Sqrt(A)/q;
+            }
 
-                double beta	=	Math.Sqrt(A)/q;
-                
-                // peaking
-                if(type==FilterType.PEAKING)
-                {
-                    b0 = (float) (1.0+alpha*A);
-                    b1 = (float) (-2.0*tcos);
-                    b2 = (float) (1.0-alpha*A);
-                    a0 = (float) (1.0+alpha/A);
-                    a1 = (float) (-2.0*tcos);
-                    a2 = (float) (1.0-alpha/A);
-                }
-                
-                // lowshelf
-                if(type==FilterType.LOWSHELF)
-                {
-                    b0 = (float) (A*((A+1.0)-(A-1.0)*tcos+beta*tsin));
-                    b1 = (float) (2.0*A*((A-1.0)-(A+1.0)*tcos));
-                    b2 = (float) (A*((A+1.0)-(A-1.0)*tcos-beta*tsin));
-                    a0 = (float) ((A+1.0)+(A-1.0)*tcos+beta*tsin);
-                    a1 = (float) (-2.0*((A-1.0)+(A+1.0)*tcos));
-                    a2 = (float) ((A+1.0)+(A-1.0)*tcos-beta*tsin);
-                }
-
-                // hishelf
-                if(type==FilterType.HISHELF)
-                {
-                    b0 = (float) (A*((A+1.0)+(A-1.0)*tcos+beta*tsin));
-                    b1 = (float) (-2.0*A*((A-1.0)+(A+1.0)*tcos));
-                    b2 = (float) (A*((A+1.0)+(A-1.0)*tcos-beta*tsin));
-                    a0 = (float) ((A+1.0)-(A-1.0)*tcos+beta*tsin);
-                    a1 = (float) (2.0*((A-1.0)-(A+1.0)*tcos));
-                    a2 = (float) ((A+1.0)-(A-1.0)*tcos-beta*tsin);
-                }
-
-            } else {
-
-                // other filters
-                double omega	=	2.0*temp_pi*frequency/Global.MixRate;
-                double tsin	=	Math.Sin(omega);
-                double tcos	=	Math.Cos(omega);
-
-                if(q_is_bandwidth)
-                alpha=tsin*Math.Sinh(Math.Log(2.0)/2.0*q*omega/tsin);
-                else
-                alpha=tsin/(2.0*q);
-
-                
-                // lowpass
-                if(type==FilterType.LOWPASS)
-                {
-                    b0=(1.0-tcos)/2.0;
-                    b1=1.0-tcos;
-                    b2=(1.0-tcos)/2.0;
-                    a0=1.0+alpha;
-                    a1=-2.0*tcos;
-                    a2=1.0-alpha;
-                }
-
-                // hipass
-                if(type==FilterType.HIPASS)
-                {
-                    b0=(1.0+tcos)/2.0;
-                    b1=-(1.0+tcos);
-                    b2=(1.0+tcos)/2.0;
-                    a0=1.0+ alpha;
-                    a1=-2.0*tcos;
-                    a2=1.0-alpha;
-                }
-
-                // bandpass csg
-                if(type==FilterType.BANDPASS_CSG)
-                {
-                    b0=tsin/2.0;
-                    b1=0.0;
-                    b2=-tsin/2;
-                    a0=1.0+alpha;
-                    a1=-2.0*tcos;
-                    a2=1.0-alpha;
-                }
-
-                // bandpass czpg
-                if(type==FilterType.BANDPASS_CZPG)
-                {
-                    b0=alpha;
-                    b1=0.0;
-                    b2=-alpha;
-                    a0=1.0+alpha;
-                    a1=-2.0*tcos;
-                    a2=1.0-alpha;
-                }
-
-                // notch
-                if(type==FilterType.NOTCH)
-                {
-                    b0=1.0;
-                    b1=-2.0*tcos;
-                    b2=1.0;
-                    a0=1.0+alpha;
-                    a1=-2.0*tcos;
-                    a2=1.0-alpha;
-                }
-
-                // allpass
-                if(type==FilterType.ALLPASS)
-                {
-                    b0=1.0-alpha;
-                    b1=-2.0*tcos;
-                    b2=1.0+alpha;
-                    a0=1.0+alpha;
-                    a1=-2.0*tcos;
-                    a2=1.0-alpha;
-                }
-
-                if(type==FilterType.NONE)
-                {
+            switch((FilterType)eg.aux_func)
+            {
+                case FilterType.NONE:
                     b0=1;
                     b1=1;
                     b2=1;
                     a0=1;
                     a1=1;
                     a2=1;
-                }
+                    break;
+                                
+                case FilterType.LOWPASS:
+                    b0=(1.0-tcos)/2.0;
+                    b1=1.0-tcos;
+                    b2=(1.0-tcos)/2.0;
+                    a0=1.0+alpha;
+                    a1=-2.0*tcos;
+                    a2=1.0-alpha;
+                    break;
+
+                case FilterType.HIPASS:
+                    b0=(1.0+tcos)/2.0;
+                    b1=-(1.0+tcos);
+                    b2=(1.0+tcos)/2.0;
+                    a0=1.0+ alpha;
+                    a1=-2.0*tcos;
+                    a2=1.0-alpha;
+                    break;
+
+                case FilterType.BANDPASS_CSG:
+                    b0=tsin/2.0;
+                    b1=0.0;
+                    b2=-tsin/2;
+                    a0=1.0+alpha;
+                    a1=-2.0*tcos;
+                    a2=1.0-alpha;
+                    break;
+
+                case FilterType.BANDPASS_CZPG:
+                    b0=alpha;
+                    b1=0.0;
+                    b2=-alpha;
+                    a0=1.0+alpha;
+                    a1=-2.0*tcos;
+                    a2=1.0-alpha;
+                    break;
+
+                case FilterType.NOTCH:
+                    b0=1.0;
+                    b1=-2.0*tcos;
+                    b2=1.0;
+                    a0=1.0+alpha;
+                    a1=-2.0*tcos;
+                    a2=1.0-alpha;
+                    break;
+
+                case FilterType.ALLPASS:
+                    b0=1.0-alpha;
+                    b1=-2.0*tcos;
+                    b2=1.0+alpha;
+                    a0=1.0+alpha;
+                    a1=-2.0*tcos;
+                    a2=1.0-alpha;
+                    break;
+
+                case FilterType.PEAKING:
+                    b0 = (float) (1.0+alpha*A);
+                    b1 = (float) (-2.0*tcos);
+                    b2 = (float) (1.0-alpha*A);
+                    a0 = (float) (1.0+alpha/A);
+                    a1 = (float) (-2.0*tcos);
+                    a2 = (float) (1.0-alpha/A);
+                    break;
+                
+                case FilterType.LOWSHELF:
+                    b0 = (float) (A*((A+1.0)-(A-1.0)*tcos+beta*tsin));
+                    b1 = (float) (2.0*A*((A-1.0)-(A+1.0)*tcos));
+                    b2 = (float) (A*((A+1.0)-(A-1.0)*tcos-beta*tsin));
+                    a0 = (float) ((A+1.0)+(A-1.0)*tcos+beta*tsin);
+                    a1 = (float) (-2.0*((A-1.0)+(A+1.0)*tcos));
+                    a2 = (float) ((A+1.0)+(A-1.0)*tcos-beta*tsin);
+                    break;
+
+                case FilterType.HISHELF:
+                    b0 = (float) (A*((A+1.0)+(A-1.0)*tcos+beta*tsin));
+                    b1 = (float) (-2.0*A*((A-1.0)+(A+1.0)*tcos));
+                    b2 = (float) (A*((A+1.0)+(A-1.0)*tcos-beta*tsin));
+                    a0 = (float) ((A+1.0)-(A-1.0)*tcos+beta*tsin);
+                    a1 = (float) (2.0*((A-1.0)-(A+1.0)*tcos));
+                    a2 = (float) ((A+1.0)-(A-1.0)*tcos-beta*tsin);
+                    break;
             }
 
             // set filter coeffs
